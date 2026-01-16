e2960ab0350ee33ebcefcc0338d8e3b599f0fab9
tas560 <tasnim.chtila@esprit.tn>
2026-01-16 11:07:02 +0100

    test

--- src/main/java/tn/esprit/studentmanagement/StudentManagementApplication.java
+++ src/main/java/tn/esprit/studentmanagement/StudentManagementApplication.java
@@ -1,13 +1,14 @@
 package tn.esprit.studentmanagement;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootApplication
-public class StudentManagementApplication {
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-    public static void main(String[] args) {
-        SpringApplication.run(StudentManagementApplication.class, args);
+public class StudentManagementApplicationTests {
+
+    @Test
+    void dummyTest() {
+        assertTrue(true);
     }
-
 }
